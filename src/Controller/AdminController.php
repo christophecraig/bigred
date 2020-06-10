3fe8f28459ebe5dd8a36ac3c5e8ca056baccd5f7
Christophe Craig <christophe.craig@viacesi.fr>
2020-06-10 20:09:05 +1200

    added orders to admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function index(SerializerInterface $serializer)
     {
+        $em = $this->getDoctrine()->getRepository(Orders::class);
+        $orders = $em->findAll();
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'orders' => $orders,
         ]);
     }
 }
