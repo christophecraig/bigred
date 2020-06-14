e3c5a9105abbe66c41e3e3c7a6a78c307b31f64a
Christophe Craig <christophe.craig@viacesi.fr>
2020-06-14 14:05:06 +1200

    facebook login (wip)

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,16 +2,50 @@
 
 namespace App\Controller;
 
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Facebook\Facebook;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AboutController extends AbstractController
 {
     /**
      * @Route("/about", name="about")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
+        $session->start();
+        $fb = new Facebook([
+            'app_id' => $_ENV['FACEBOOK_APP_ID'],
+            'app_secret' => $_ENV['FACEBOOK_APP_SECRET'],
+            'default_graph_version' => 'v7.0',
+            'default_access_token' => $_ENV['FACEBOOK_APP_TOKEN'],
+        ]);
+
+        $helper = $fb->getRedirectLoginHelper();
+
+        $permissions = ['email', 'public_profile', 'pages_messaging']; // Optional permissions
+        $callbackUrl = htmlspecialchars(
+            'https://dev.christophecraig.com/facebook'
+        );
+        $loginUrl = $helper->getLoginUrl($callbackUrl, $permissions);
+
+        echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+
+        try {
+            // Returns a `FacebookFacebookResponse` object
+            $response = $fb->get('/me');
+        } catch (FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit();
+        } catch (FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit();
+        }
+        $graphNode = $response->getGraphNode();
+
         return $this->render('about/index.html.twig', [
             'controller_name' => 'AboutController',
         ]);
---
+++ src/Controller/FacebookController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FacebookController extends AbstractController
+{
+    /**
+     * @Route("/facebook", name="facebook")
+     */
+    public function index(Request $request, SessionInterface $session)
+    {
+        $session->start();
+        $fb = new Facebook([
+            'app_id' => $_ENV['FACEBOOK_APP_ID'],
+            'app_secret' => $_ENV['FACEBOOK_APP_SECRET'],
+            'default_graph_version' => 'v7.0',
+            'persistent_data_handler' => 'session',
+        ]);
+
+        $helper = $fb->getRedirectLoginHelper();
+
+        try {
+            $accessToken = $helper->getAccessToken($this->generateUrl('about'));
+        } catch (FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit();
+        } catch (FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit();
+        }
+
+        if (!isset($accessToken)) {
+            if ($helper->getError()) {
+                header('HTTP/1.0 401 Unauthorized');
+                echo 'Error: ' . $helper->getError() . "\n";
+                echo 'Error Code: ' . $helper->getErrorCode() . "\n";
+                echo 'Error Reason: ' . $helper->getErrorReason() . "\n";
+                echo 'Error Description: ' .
+                    $helper->getErrorDescription() .
+                    "\n";
+            } else {
+                header('HTTP/1.0 400 Bad Request');
+                echo 'Bad request';
+            }
+            exit();
+        }
+
+        // Logged in
+        echo '<h3>Access Token</h3>';
+        var_dump($accessToken->getValue());
+
+        // The OAuth 2.0 client handler helps us manage access tokens
+        $oAuth2Client = $fb->getOAuth2Client();
+
+        // Get the access token metadata from /debug_token
+        $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+        echo '<h3>Metadata</h3>';
+        var_dump($tokenMetadata);
+
+        // Validation (these will throw FacebookSDKException's when they fail)
+        $tokenMetadata->validateAppId($_ENV['FACEBOOK_APP_ID']); // Replace {app-id} with your app id
+        // If you know the user ID this access token belongs to, you can validate it here
+        //$tokenMetadata->validateUserId('123');
+        $tokenMetadata->validateExpiration();
+
+        if (!$accessToken->isLongLived()) {
+            // Exchanges a short-lived access token for a long-lived one
+            try {
+                $accessToken = $oAuth2Client->getLongLivedAccessToken(
+                    $accessToken
+                );
+            } catch (FacebookSDKException $e) {
+                echo '<p>Error getting long-lived access token: ' .
+                    $e->getMessage() .
+                    "</p>\n\n";
+                exit();
+            }
+
+            echo '<h3>Long-lived</h3>';
+            var_dump($accessToken->getValue());
+        }
+
+        $this->$_SESSION['fb_access_token'] = (string) $accessToken;
+        // User is logged in with a long-lived access token.
+        // You can redirect them to a members-only page.
+
+        return $this->redirectToRoute('about');
+        return $this->render('facebook/index.html.twig', [
+            'controller_name' => 'FacebookController',
+        ]);
+    }
+}
