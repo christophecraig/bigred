30703edd4b709c62ba7e3f47d7fd1fb761547903
Christophe Craig <christophe.craig@viacesi.fr>
2020-06-17 18:09:18 +1200

    after reading controllers doc i feel stupid

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -13,18 +13,21 @@ class WebhookController extends AbstractController
     /**
      * @Route("/webhook", name="webhook", methods={"GET"})
      */
-    public function index(LoggerInterface $logger)
+    public function index()
     {
+        $response = new Response();
         $verifyToken = 'sublime1234';
-        $logger->debug('$_GET: ', $_GET);
+        // $logger->debug('$_GET: ', $_GET);
         if (
             $_GET['hub.mode'] === 'subscribe' &&
             $_GET['hub.verify_token'] === $verifyToken
         ) {
-            $logger->debug('yessss');
-            return new Response($_GET['hub.challenge'], 200);
+            // $logger->debug('yessss');
+            $response->setContent($_GET['hub.challenge']);
+            $response->setStatusCode(200);
+            return $response;
         } else {
-            http_response_code(403);
+            return $response->setStatusCode(403);
         }
     }
 }
