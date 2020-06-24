dd3c14dfd682c4d9829950af48d9d53747f35128
Christophe Craig <christophe.craig@viacesi.fr>
2020-06-24 18:23:12 +1200

    trying to send confirmation on order

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -14,6 +14,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Exceptions\FacebookResponseException;
 
 /**
  * @Route("/orders")
@@ -82,7 +85,41 @@ class OrdersController extends AbstractController
                     'user' => $this->getUser(),
                 ]);
             $mailer->send($email);
-
+            $fb = new Facebook([
+                'app_id' => $_ENV['FACEBOOK_APP_ID'],
+                'app_secret' => $_ENV['FACEBOOK_APP_SECRET'],
+                'default_graph_version' => 'v7.0',
+                'default_access_token' => $_ENV['FACEBOOK_APP_TOKEN'],
+                'persistent_data_handler' => 'session',
+            ]);
+            // Send confirmation to messenger
+            $message = 'Your order is now waiting for confirmation';
+            try {
+                // Returns a `FacebookFacebookResponse` object
+                $response = $fb->post(
+                    '/me/messages',
+                    [
+                        'messaging_type' => 'UPDATE',
+                        'recipient' => '{
+                          "id": "2754187091352898"
+                        }',
+                        'message' =>
+                            '{
+                          "text": "' .
+                            $message .
+                            '"
+                        }',
+                    ],
+                    'EAAmZCJ9U8z1YBAF63doj3ZACfZAcgoplwDbiDZCcetZAeofZBXwTDhox8ZCxBcmJPmPwZAPZBU3Oy3q4FZABCkZCnlFwo4UmEEZCfKfhMtdtndAP0YYfQ1Ad6kErmkyuTrlo5ftSA1i8GqpjgZCk0TaWWEmodlAaYoibdzkZAR9w56iKe8xJSGlT40nkqv8qghX3HzL9AZD'
+                );
+                return new Response('', 200);
+            } catch (FacebookResponseException $e) {
+                echo 'Graph returned an error: ' . $e->getMessage();
+                exit();
+            } catch (FacebookSDKException $e) {
+                echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                exit();
+            }
             return $this->redirectToRoute('home');
         }
 
