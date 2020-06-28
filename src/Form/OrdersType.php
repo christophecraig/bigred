6ff461416dd1e4deb813fd10ccc39c4248a0c196
Christophe Craig <christophe.craig@viacesi.fr>
2020-06-28 16:59:30 +1200

    removed useless field when rescheduling order

--- src/Form/OrdersType.php
+++ src/Form/OrdersType.php
@@ -9,28 +9,27 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class OrdersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // TODO: Change to radio button for deliveryTime
-        $builder
-            ->add('deliveryDate')
-            ->add('deliveryTime', ChoiceType::class, [
-                'choices' => [
-                    'Morning' => 'morning',
-                    'Afternoon' => 'afternoon',
-                ],
-            ])
-            ->add('paymentType', ChoiceType::class, [
-                'choices' => [
-                    'Cash' => Orders::PAYMENT_CASH,
-                    'Direct Credit' => Orders::PAYMENT_DIRECT_CREDIT,
-                ],
-            ])
-            ->add('comment', TextareaType::class);
+        $builder->add('deliveryDate')->add('deliveryTime', ChoiceType::class, [
+            'choices' => [
+                'Morning' => 'morning',
+                'Afternoon' => 'afternoon',
+            ],
+        ]);
+        if (null === $builder->getData()->getId()) {
+            $builder
+                ->add('paymentType', ChoiceType::class, [
+                    'choices' => [
+                        'Cash' => Orders::PAYMENT_CASH,
+                        'Direct Credit' => Orders::PAYMENT_DIRECT_CREDIT,
+                    ],
+                ])
+                ->add('comment', TextareaType::class);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
