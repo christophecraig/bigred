9f203f928835fe9afc1d8c4218fcbcce39733fb2
Christophe Craig <christophe.craig@viacesi.fr>
2020-06-29 13:08:56 +1200

    updated order forms

--- src/Form/OrdersType.php
+++ src/Form/OrdersType.php
@@ -5,7 +5,8 @@ namespace App\Form;
 use App\Entity\Orders;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,21 +15,34 @@ class OrdersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('deliveryDate')->add('deliveryTime', ChoiceType::class, [
-            'choices' => [
-                'Morning' => 'morning',
-                'Afternoon' => 'afternoon',
-            ],
-        ]);
+        //creation
         if (null === $builder->getData()->getId()) {
             $builder
+                ->add('deliveryDate', DateType::class, [
+                    'widget' => 'single_text',
+                    'attr' => ['style' => 'visibility: hidden'],
+                ])
+                ->add('deliveryTime', HiddenType::class)
                 ->add('paymentType', ChoiceType::class, [
                     'choices' => [
                         'Cash' => Orders::PAYMENT_CASH,
                         'Direct Credit' => Orders::PAYMENT_DIRECT_CREDIT,
                     ],
                 ])
-                ->add('comment', TextareaType::class);
+                ->add('comment', TextareaType::class, [
+                    'attr' => ['class' => 'textarea'],
+                ]);
+        } else {
+            $builder
+                ->add('deliveryDate', DateType::class, [
+                    'widget' => 'single_text',
+                ])
+                ->add('deliveryTime', ChoiceType::class, [
+                    'choices' => [
+                        'Morning' => 'morning',
+                        'Afternoon' => 'afternoon',
+                    ],
+                ]);
         }
     }
 
